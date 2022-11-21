07b5041b63fcdc93dee35856925c22c96aa34244
nehaps2000 <neha.ps@gadgeon.com>
2022-11-21 12:15:32 +0530

    root update

--- src/pages/ProjectSettings.js
+++ src/pages/ProjectSettings.js
@@ -1,33 +1,70 @@
-import {useState} from 'react'
+import { useState } from "react";
+import Allocations from "./Allocations";
+import Resources from "./Resources";
+import Team from "./Team";
+import Hamburger from "hamburger-react";
+import { useParams } from "react-router-dom";
 
-import Hamburger from 'hamburger-react';
-import { Link } from "react-router-dom";
 const ProjectSettings = () => {
-    
-    const [isOpen, setOpen] = useState(false)
-    return (<>
-        <Hamburger toggled={isOpen} toggle={setOpen} />
-        <div class="card text-center">
-  <div class="card-header">
-    <ul class="nav nav-tabs card-header-tabs">
-      <li class="nav-item">
-        <Link to="/Allocations" class="nav-link active" >Allocations</Link>
-      </li>
-      <li class="nav-item">
-        <Link to="/Resources" class="nav-link active" >Resources</Link>
-      </li>
-      <li class="nav-item">
-        <Link to ="/Team"class="nav-link active" >Team</Link>
-      </li>
-    </ul>
-  </div>
-  <div class="card-body">
-    <h5 class="card-title">..........................</h5>
-    <p class="card-text">.............................</p>
-  </div>
-</div>
-    </>  )
-  };
+  const [isOpen, setOpen] = useState(false);
+  const [selectedTab, setSelectedTab] = useState(null);
 
+  const params = useParams();
+  console.log(params);
 
-export default ProjectSettings
+  return (
+    <>
+      <Hamburger toggled={isOpen} toggle={setOpen} />
+
+      <div class="card text-center">
+        <div class="card-header">
+          <ul class="nav nav-tabs card-header-tabs">
+            <li 
+              class="nav-item"
+              onClick={() => {
+                setSelectedTab("Allocations");
+              }}
+            >
+           Allocations 
+            </li>
+            <li
+              class="nav-item"
+              onClick={() => {
+                setSelectedTab("Resources");
+              }}
+            >
+              Resources
+            </li>
+            <li
+              class="nav-item"
+              onClick={() => {
+                setSelectedTab("Team");
+              }}
+            >
+              Team
+            </li>
+          </ul>
+        </div>
+        <div class="card-body">
+          {/* <h5 class="card-title">..........................</h5>
+          <p class="card-text">.............................</p> */}
+          {selectedTab === "Allocations" ? (
+            <div>
+              <Allocations></Allocations>
+            </div>
+          ) : selectedTab === "Resources" ? (
+            <div>
+              <Resources></Resources>
+            </div>
+          ) : (
+            <div>
+              <Team></Team>
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ProjectSettings;
