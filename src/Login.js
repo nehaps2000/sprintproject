f89e7e8af9071ad8a48fe44c9cdde3e88d390eeb
nehaps2000 <neha.ps@gadgeon.com>
2022-12-05 11:38:23 +0530

    login_authorization

--- src/Login.js
+++ src/Login.js
@@ -1,17 +1,49 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
+import api from "./utility/api";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const Navigate = useNavigate();
 
-  const Submit = () => {
-    Navigate("/Project");
+   useEffect(()=>{
+    if(!localStorage.getItem('token')){
+      Navigate("/project");
+    }
+   })
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
   };
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+  const handleApi = async () => {
+    console.log({ email, password });
+    let response = await api("post", "/api/Auth/Login", {
+      username: email,
+      password: password,
+      
+    });
+   
+      if (response!=="Wrong Credentials!")
+      {
 
+      console.log(response);
+
+        localStorage.setItem("token", response);
+
+        Navigate("/project");
+      }
+      // })
+      // .catch((error) => {
+      //   alert("service error");
+      //   console.log(error);
+      // });
+    
+  };
   return (
     <>
       <div className="container">
@@ -22,24 +54,26 @@ const Login = () => {
               <label>Email</label>
               <input
                 type="email"
+                value={email}
                 className="form-control mt-1"
                 placeholder="email"
-                onChange={(e) => setEmail(e.target.value)}
+                onChange={handleEmail}
               />
             </div>
             <div className="form-group mt-3">
               <label>Password</label>
               <input
                 type="password"
+                value={password}
                 className="form-control mt-1"
                 placeholder="password"
-                onChange={(e) => setPassword(e.target.value)}
+                onChange={handlePassword}
               />
             </div>
             <div className="d-grid gap-2 mt-3">
               <button
-                type="submit"
-                onClick={Submit}
+                type="button"
+                onClick={handleApi}
                 className="btn btn-primary"
               >
                 Submit
@@ -57,3 +91,4 @@ const Login = () => {
 };
 
 export default Login;
+//.split('.').length === 3
