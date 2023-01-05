5fe95f0b9c0d20a76236602c2e96df1747d0094b
nehaps2000 <neha.ps@gadgeon.com>
2023-01-05 18:30:26 +0530

    navbar update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,14 +6,16 @@ const Navbar = () => {
   const params = useParams();
   console.log(params);
   const url = "/api/Project/Projects";
-
+  const [userName, setuserName] = useState("");
   const [projectName, setProjectName] = useState("");
   const Navigate = useNavigate();
 
   useEffect(() => {
     setProjectName(localStorage.getItem(params.Id));
   });
-
+  useEffect(() => {
+    setuserName(localStorage.getItem("username"));
+  });
   const logout = (e) => {
     console.log("Logout");
     const confirm = alert("are you sure you want to logout?");
@@ -23,7 +25,10 @@ const Navbar = () => {
   return (
     <>
       <div className="header">
-        <div className="name"><h1>{projectName}</h1></div>
+        <div className="name">
+          <h1>{projectName}</h1>
+        </div>
+
         <div>
           <div className="logout">
             <Logout
@@ -33,6 +38,7 @@ const Navbar = () => {
             />
           </div>
         </div>
+        <div className="user"><h5>welcome {userName}!</h5></div>
       </div>
     </>
   );
