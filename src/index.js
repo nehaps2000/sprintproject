baa2aa2ecd5ca46dbc2596669374c8482e81f722
nehaps2000 <neha.ps@gadgeon.com>
2023-01-11 15:07:08 +0530

    Storylog changes

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
  import App from './App';
+import { ToastContainer } from 'react-toastify';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ToastContainer position="top-right" autoClose={5000} hideProgressBar={true} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable
+pauseOnHover theme="dark" />
     <App />
   </React.StrictMode>
 );
--- src/pages/ListedStory.js
+++ src/pages/ListedStory.js
@@ -1,11 +1,49 @@
+import React from "react";
 
+import api from "../utility/api";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
+import Navbar from "../components/Navbar";
+import Hamburger from "../components/Hamburger";
 const ListedStory = () => {
-    return (  
-        <div>
-            listed story
-        </div>
-    );
-}
- 
+  const params = useParams();
+
+ // let id = localStorage.getItem("sprintId");
+
+  const url = `/api/Story/GetAddedStories/${params.Id}`;
+  const [viewList, setViewList] = useState([]);
+
+  console.log(params.Id, "params");
+  useEffect(() => {
+    const apiCall = async () => {
+      let response = await api("get", url);
+      setViewList(response);
+    };
+    apiCall();
+  }, [url]);
+
+  return (
+    <><Navbar></Navbar>
+    <Hamburger></Hamburger>
+    <div className="selected"><h1>Selected Stories</h1></div>
+        <table className="display">
+   
+      <tr>
+        <th>Name</th>
+        <th>ID</th>
+      </tr>
+      {viewList?.map((story) => {
+            return (
+              <tr key={story.name}>
+                <td> {story.name}</td>
+                <td> {story.id}</td>
+        
+              </tr>
+            );
+          })}
+    </table>
+  
+</>  );
+};
 export default ListedStory;
